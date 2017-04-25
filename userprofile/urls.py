8c02c97fcd7a201267ae5593008f49b4e3df8ef0
faithNassiwa <faithnassiwa@gmail.com>
2017-04-25 20:00:47 +0300

    updated login template

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -3,7 +3,8 @@ from django.contrib.auth import views as auth_views
 from userprofile import views as user_views
 
 urlpatterns = [
-    url(r'^login/', auth_views.login, name='login', kwargs={'redirect_authenticated_user': True}),
+    url(r'^login/', auth_views.login, name='login',  kwargs={'redirect_authenticated_user': True,
+                                                             'template_name': 'userprofile/login.html'}),
     url(r'^accounts/verified_registration/(?P<medic_id>[0-9]+)$', user_views.register, name='register'),
     url(r'^accounts/unverified_registration/$', user_views.unverified_register, name='unverified_register'),
     url(r'^$', user_views.verify, name='verify'),
