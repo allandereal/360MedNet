555283df34f82cad9af51e97d75e54ce74f0a86a
allandereal <ahullan@gmail.com>
2017-08-26 22:50:16 +0300

    first commit

--- MedNet/wsgi.py
+++ MedNet/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MedNet.settings.test")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MedNet.settings.dev")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
